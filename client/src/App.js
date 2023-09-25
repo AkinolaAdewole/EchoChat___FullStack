c230140193dce7ca121541025381f55bb06b284c
Akinola Adewole <adewoleakinola24@gmail.com>
2023-09-25 15:34:13 +0100

    second commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import Join from './components/Join';
+import {Routes, Route } from "react-router-dom";
+import Join from './components/join/Join';
 import Chat from './components/Chat';
 
 function App() {
   return (
    <>
-      <Router>
+      <Routes>
         <Route path='/' exact Component={Join} />
         <Route path='/client' Component={Chat} />
-      </Router>
+      </Routes>
    </>
   );
 }
