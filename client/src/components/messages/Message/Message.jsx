273b4273dbbb58bb6bec343a5af5ce128f1dec37
Akinola Adewole <adewoleakinola24@gmail.com>
2023-09-25 17:47:32 +0100

    second commit

---
+++ client/src/components/messages/Message/Message.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import './Message.css';
+import ReactEmoji from 'react-emoji';
+
+const Message = () => {
+  return (
+    <div>Message</div>
+  )
+}
+
+export default Message
