2f9b317e2ff8b961e9c45e7f65bf066303af6597
Akinola Adewole <adewoleakinola24@gmail.com>
2023-09-25 18:21:09 +0100

    second commit

--- client/src/components/chat/Chat.jsx
+++ client/src/components/chat/Chat.jsx
@@ -9,11 +9,14 @@ import Input from '../input/Input';
 
 import './chat.css';
 
+// Define the endpoint for the socket.io server (replace with your actual server address)
 const ENDPOINT = 'http:localhost';
 
 let socket;
 
+// Define the Chat component
 const Chat = ({ location }) => {
+  // Initialize state variables using the useState hook
   const [name, setName] = useState('');
   const [room, setRoom] = useState('');
   const [users, setUsers] = useState('');
@@ -21,48 +24,61 @@ const Chat = ({ location }) => {
   const [messages, setMessages] = useState([]);
 
   useEffect(() => {
+    // Parse the 'name' and 'room' from the URL query parameters
     const { name, room } = queryString.parse(location.search);
 
+    // Initialize a socket.io client connection to the server
     socket = io(ENDPOINT);
 
+    // Set the 'room' and 'name' state variables based on the parsed values
     setRoom(room);
-    setName(name)
+    setName(name);
 
+    // Emit a 'join' event to the server, providing user details and handling errors
     socket.emit('join', { name, room }, (error) => {
-      if(error) {
-        alert(error);
+      if (error) {
+        alert(error); // Display an alert if there's an error
       }
     });
   }, [ENDPOINT, location.search]);
-  
+
   useEffect(() => {
+    // Listen for incoming 'message' events from the server and update the 'messages' state
     socket.on('message', message => {
-      setMessages(messages => [ ...messages, message ]);
+      setMessages(messages => [...messages, message]);
     });
-    
+
+    // Listen for 'roomData' events from the server and update the 'users' state
     socket.on("roomData", ({ users }) => {
       setUsers(users);
     });
-}, []);
+  }, []);
 
+  // Function to send a message to the server
   const sendMessage = (event) => {
     event.preventDefault();
 
-    if(message) {
+    if (message) {
+      // Emit a 'sendMessage' event to the server with the message content
       socket.emit('sendMessage', message, () => setMessage(''));
     }
   }
 
+
   return (
     <div className="outerContainer">
       <div className="container">
-          <InfoBar room={room} />
-          <Messages messages={messages} name={name} />
-          <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
+        {/* InfoBar component displaying room information */}
+        <InfoBar room={room} />
+        {/* Messages component displaying chat messages */}
+        <Messages messages={messages} name={name} />
+        {/* Input component for typing and sending messages */}
+        <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
       </div>
-      <TextContainer users={users}/>
+      {/* TextContainer component displaying a list of users in the chat room */}
+      <TextContainer users={users} />
     </div>
   );
 }
 
-export default Chat;
+export default Chat; // Export the Chat component for use in other parts of the application
